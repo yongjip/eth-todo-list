e7dfaa445dcaa66e7139df41e13658dd634c8835
yongjip <yongjip@coupang.com>
2022-02-23 00:05:11 +0900

    finally succeeded to connect to metamask wallet

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id; // uint = int that can't be negative
+        string content;
+        bool completed;
+    }
+
+    // unit: data type for ID
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Cehck our dappuniversity.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content,false);
+    }
+
 }
